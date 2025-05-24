402bf9889561b48e448a761b9071414c016e2768
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-05-24 19:37:56 +0300

    Enhances movie search UI and integrates state management
    Introduces `useState` to manage search term and movie results.
    Replaces search icon with `CiSearch` for a modern look and updates
    UI layout with form enhancements and input grouping for better usability.
    Restores `MovieList` rendering for displaying search results.

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,15 +1,28 @@
-import Css from './MoviesPage.module.css';
+import { useState } from 'react';
 
+import Css from './MoviesPage.module.css';
+// import { FaSearch } from "react-icons/fa";
+import { CiSearch } from "react-icons/ci";
 const MoviesPage = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchedMovies, setSearchedMovies] = useState();
     return (
         <div className={Css.MoviesPage}>
             <div className={Css.MoviesPageCard}>
                 <form action="/movies" className={Css.Form}>
-                    <input className={Css.Input} name='filmName' type="text" placeholder="Search for a movie..." />
-                    <button className={Css.Btn} type="submit">Search</button>
+                    <div className={Css.InputGroup}>
+                        <CiSearch color='currentcolor' size={24} />
+                        <input className={Css.Input} name='filmName' type="text" placeholder="Search for a movie..." />
+                    </div>
+
+                    <button className={Css.Btn} type="submit">
+                        <CiSearch color='currentcolor' size={40} > Search </CiSearch> </button>
                 </form>
             </div>
-            {/* <MovieList data={movies} /> */}
+            <div className={Css.MoviesPageCard}>
+                <MovieList data={movies} />
+            </div>
+
         </div>
     );
 }
