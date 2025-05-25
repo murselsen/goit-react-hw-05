d9068e3a0d76b4c65f5721d1fc03508fbb75768b
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-05-25 23:04:16 +0300

    Refactor MovieDetailsPage component and styles for improved layout and data presentation

--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import getMovieById from '../utils/getMovieById';
 // Css 
-import Css from './MoviesPage.module.css';
+import Css from './MoviesDetailsPage.module.css';
 
 const MovieDetailsPage = () => {
 
@@ -24,26 +24,55 @@ const MovieDetailsPage = () => {
 
     }, [id]);
 
-    const { title, poster_path, overview } = movie || {};
+    const { title, poster_path, overview, vote_average, vote_count, genres } = movie || {};
     return (
         console.log("Movie: ", movie),
         <div className={Css.MoviesPage}>
-            <div className={[Css.MoviesPageCard, Css.MovieDetailsCard].join(' ')}>
+            <div className={Css.MovieDetailsCard}>
                 <div className={Css.Media}>
                     <img src={'https://image.tmdb.org/t/p/w500' + poster_path} />
                 </div>
                 <div className={Css.Content}>
-                    <div className={Css.ContentItem}>
+                    <h1 className={Css.Title}>{title}</h1>
+                    <p className={Css.Description}>
+                        {
+                            overview ? overview : 'No description available'
+                        }
+                    </p>
+
+                    {/*   <div className={Css.ContentItem}>
                         <label className={Css.Label}>Title</label>
                         <h1 className={[Css.Title, Css.Badge].join(' ')}>
                             {title}
                         </h1>
                     </div>
+                  
+                    <div className={Css.ContentItem}>
+                        <label className={Css.Label}>Vote</label>
+                        <h4 className={[Css.Title, Css.Badge].join(' ')}>
+                            {vote_count}
+                        </h4>
+                    </div>
+                    <div className={Css.ContentItem}>
+                        <label className={Css.Label}>Overview</label>
+                        <p className={[Css.Description, Css.Badge].join(' ')}>
+                            {overview}
+                        </p>
+                    </div>
+                    <div className={Css.ContentItem}>
+                        <label className={Css.Label}>Genres</label>
+                        <div className={Css.Badges}>
 
-                    <p className={Css.Description}>{overview}</p>
+                            {genres ? genres.map((genre) => <span key={genre.id}>{genre.name}</span>) : <span>No genres available</span>}
+
+                        </div>
+                    </div>
+ */}
+
+
                 </div>
             </div>
-        </div >
+        </div>
     );
 
 }
